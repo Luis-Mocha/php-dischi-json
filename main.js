26d559396685716c9b5ad6fada6ba2a044464801
Luis-Mocha <luismocha97@gmail.com>
2023-05-25 15:57:02 +0200

    setting iniziale vue cdn

---
+++ main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue 
+
+createApp ({
+    data() {
+        return {
+            data: 'prova prova',
+            
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        
+    },
+
+}).mount('#app')
